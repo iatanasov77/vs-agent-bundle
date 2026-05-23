a83a2867680c3b4f50c71961c457bdb932c59016
Ivan I. Atanasov <i.atanasov77@gmail.com>
2026-05-23 04:39:33 +0000

    Create Broke Virtual Host Action.

--- src/Vankosoft/AgentBundle/Controller/ActionsController.php
+++ src/Vankosoft/AgentBundle/Controller/ActionsController.php
@@ -3,6 +3,8 @@
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Vankosoft\ApplicationBundle\Component\Status;
 
 class ActionsController extends AbstractController
 {
@@ -10,6 +12,18 @@ class ActionsController extends AbstractController
     {
         return $this->render( '@VSAgent/Pages/Actions/index.html.twig', [
             
+        ]);
+    }
+    
+    public function brokeVirtualHost( Request $request ): Response
+    {
+        $filecontents = \file_get_contents( "/etc/httpd/conf.d/25-myprojects.lh.conf" );
+        
+        return new JsonResponse([
+            'status'    => Status::STATUS_OK,
+            'data'      => [
+                'filecontents' => $filecontents,
+            ]
         ]);
     }
 }
--- src/Vankosoft/AgentBundle/Resources/themes/default/assets/js/pages/actions-index.js
+++ src/Vankosoft/AgentBundle/Resources/themes/default/assets/js/pages/actions-index.js
@@ -1,7 +1,29 @@
 // bin/console fos:js-routing:dump --format=json --target=public/shared_assets/js/fos_js_routes_admin.json
 import { VsPath } from '@@/js/includes/fos_js_routes.js';
+import { VsSpinnerShow, VsSpinnerHide } from '@@/js/includes/vs_spinner.js';
 
 $( function()
 {
-	
+	$( '#btnBrokeVirtualHost' ).on( 'click', function ( e )
+    {
+        VsSpinnerShow();
+        $.ajax({
+            type: 'GET',
+            url: VsPath( 'vs_agent_actions_broke_virtual_host' ),
+            success: function ( response ) {
+                VsSpinnerHide();
+                
+                if ( response.status == 'ok' ) {
+                    alert( response.data.filecontents );
+                } else {
+                    alert( 'RESPONSE ERROR!!!' );
+                }
+            }, 
+            error: function(XMLHttpRequest, textStatus, errorThrown) {
+                VsSpinnerHide();
+                
+                alert( 'FATAL ERROR!!!' );
+            }
+        });
+    });
 });
